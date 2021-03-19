51126636795c3d73f2985f2c5a1394fd46f4950b
GodfredAsa <77088001+GodfredAsa@users.noreply.github.com>
2021-03-19 16:25:29 +0000

    Commited worked files on MiniProject

---
+++ Mini Project Web Banking/scripts/script.js
@@ -0,0 +1,42 @@
+function getInputValue() { 
+        let inputVal = document.getElementById("inputId").value;
+        // Displaying the value
+        var amt = 100;
+        if(inputVal == ""){
+           alert("\nDeposit Unsuccessful\nCurrent Balance: GHC " + amt)
+        }
+        else{
+           var input = Number(inputVal) + 100;
+           alert("Deposit Successful \n" +"Deposited Amount: GHC "+ inputVal +"\nPrevious Balance: GHC " + amt + "\nCurrent Balance GHC: " + input);
+        }
+      }
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
